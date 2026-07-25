bf08c54aba016d31dd7c7aee7f44fd36a6b484fd
Mufazzel Hossain <mufazzelanis@gmail.com>
2026-07-25 23:55:50 +0600

    update

--- app/Http/Controllers/SecurityController.php
+++ app/Http/Controllers/SecurityController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\LoginActivity;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class SecurityController extends Controller
 {
     public function index()
     {
-        $activities = LoginActivity::where('user_id', auth()->id())
-            ->latest('created_at')
-            ->take(20)
-            ->get();
+        $activities = collect();
+
+        if (Schema::hasTable('login_activities')) {
+            $activities = LoginActivity::where('user_id', auth()->id())
+                ->latest('created_at')
+                ->take(20)
+                ->get();
+        }
+
         return view('account.security.index', compact('activities'));
     }
 }
